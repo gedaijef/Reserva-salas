0c428609ae5102b734c8951b177a73c3625eba77
Beatriz Belaparte Favero <beatrizfavero-ieg@instituto.ad>
2024-06-17 16:35:58 -0300

    Adicionando recorrencia

--- js/reservas.js
+++ js/reservas.js
@@ -1,20 +1,14 @@
-//Essa parte serve para colocar a imagem lateral
-const barra = document.getElementById('barra-lateral')
-const img = document.createElement("img")
-const caminho = sessionStorage.getItem("img")
-img.setAttribute("src", caminho)
-img.setAttribute("alt", "Imagem da barra lateral")
-barra.appendChild(img)
+const barra = document.getElementById('barra-lateral');
+const caminho = sessionStorage.getItem("img");
+barra.innerHTML = `<img src="${caminho}" alt="Imagem da barra lateral">`;
 
-
-//Nessa lista vai ficar o resumo do que o usuário selecionou.
+// Nessa lista vai ficar o resumo do que o usuário selecionou.
 let reserva = []
 let dataSelecionada = ""
 
-
-//Funcionamento do calendário
-const daysTag = document.querySelector(".days"), //Seleciona o elemento HTML que contém os dias do calendário.
-    currentDate = document.querySelector(".current-date"), //Seleciona o elemento HTML que mostra a data atual (mês e ano).
+// Funcionamento do calendário
+const daysTag = document.querySelector(".days"), // Seleciona o elemento HTML que contém os dias do calendário.
+    currentDate = document.querySelector(".current-date"), // Seleciona o elemento HTML que mostra a data atual (mês e ano).
     prevNextIcon = document.querySelectorAll(".icons span"), // Seleciona todos os elementos de ícone para navegação (anterior e próximo).
     prevIcon = document.getElementById("prev"), // Seleciona o ícone anterior especificamente
     nextIcon = document.getElementById("next"); // Seleciona o ícone próximo especificamente
@@ -23,108 +17,90 @@ const daysTag = document.querySelector(".days"), //Seleciona o elemento HTML que
 let date = new Date(),
     currYear = date.getFullYear(),
     currMonth = date.getMonth(),
-    currentYear = currYear, // armazenando o ano atual para restringir a navegação
-    currentMonth = currMonth; // armazenando o mês atual para restringir a navegação
-
+    currentYear = currYear, // Armazenando o ano atual para restringir a navegação
+    currentMonth = currMonth; // Armazenando o mês atual para restringir a navegação
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // pegando o primeiro dia do mês
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // pegando o último dia do mês
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // pegando o último dia do mês anterior
+const calendarioInicio = () => {
+    const firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) { // criando os dias inativos do mês anterior
+    for (let i = firstDayofMonth; i > 0; i--) {
         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // criando os dias do mês atual
-        // adicionando a classe ativa ao dia se o dia atual, mês e ano corresponderem
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-            && currYear;
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
+        const dayOfWeek = new Date(currYear, currMonth, i).getDay();
+        const inactiveClass = (dayOfWeek === 0 || dayOfWeek === 6) ? "inactive" : "";
 
-        // Verifica se o dia é sábado (6) ou domingo (0)
-        let dayOfWeek = new Date(currYear, currMonth, i).getDay();
-        let inactiveClass = (dayOfWeek === 0 || dayOfWeek === 6) ? "inactive" : "";
-
-        liTag += `<li class="${isToday} ${inactiveClass}">${i}</li>`;
+        liTag += `<li class="${inactiveClass}">${i}</li>`;
     }
-
-    // preenchendo os dias restantes para completar as 6 linhas
-    for (let i = lastDayofMonth; i < 6 * 7 - (firstDayofMonth + lastDateofMonth); i++) {
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
-    }
-
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passando o mês e ano atuais como texto da currentDate
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
 
-    // Esconder ou mostrar o ícone anterior com base no mês e ano atuais
-    if (currYear === currentYear && currMonth <= currentMonth) {
-        prevIcon.classList.add("hidden");
-    } else {
-        prevIcon.classList.remove("hidden");
-    }
+    prevIcon.classList.toggle("hidden", currYear === currentYear && currMonth <= currentMonth);
+    nextIcon.classList.toggle("hidden", currYear === currentYear && currMonth === 11);
 
-    // Esconder ou mostrar o ícone próximo com base no mês e ano atuais
-    if (currYear === currentYear && currMonth === 11) {
-        nextIcon.classList.add("hidden");
-    } else {
-        nextIcon.classList.remove("hidden");
-    }
-
-    // Deixando as bolinhas coloridinhas ao clique e adicionando o dia selecionado à lista
-    const listaDias = document.getElementsByClassName("days")[0];
-    const dias = listaDias.querySelectorAll("li");
-
-    for (let i = 0; i < dias.length; i++) {
-        dias[i].addEventListener('click', () => {
-            reserva = []
-
-            if (!dias[i].classList.contains("inactive")) {
-                // Remove a classe "active" de todos os dias
-                dias.forEach(dia => dia.classList.remove("active"));
-                // Adiciona a classe "active" ao dia clicado
-                dias[i].classList.toggle("active");
-                reserva.push(dias[i].textContent)
-
-                // Exibe um alerta com a data selecionada
-                dataSelecionada = `${dias[i].textContent}/${currMonth + 1}/${currYear}`
+    document.querySelectorAll(".days li").forEach(day => {
+        day.addEventListener('click', () => {
+            if (!day.classList.contains("inactive")) {
+                document.querySelectorAll(".days li").forEach(d => d.classList.remove("active"));
+                day.classList.add("active");
+                reserva = [day.textContent];
+                dataSelecionada = `${day.textContent}/${currMonth + 1}/${currYear}`;
+                atualizarInputData();
             }
         });
+    });
+}
+
+const atualizarInputData = () => {
+    const dataSpan = document.querySelector(".diaSelecionado").textContent.trim();
+    if (dataSpan) {
+        const [dia, mes, ano] = dataSpan.split('/').map(Number);
+        const dataInicio = new Date(ano, mes - 1, dia);
+        const dataMin = new Date(dataInicio);
+        dataMin.setDate(dataMin.getDate() + 1);
+        const dataMax = new Date(dataInicio);
+        dataMax.setDate(dataMax.getDate() + 30);
+        const hoje = new Date();
+
+        const inputDate = document.querySelector('input[type="date"]');
+        inputDate.min = dataMin.toISOString().split('T')[0];
+        inputDate.max = dataMax.toISOString().split('T')[0];
+
     }
 }
 
-renderCalendar();
+calendarioInicio();
 
-prevNextIcon.forEach(icon => { // obtendo os ícones de anterior e próximo
-    icon.addEventListener("click", () => { // adicionando evento de clique em ambos os ícones
-        // se o ícone clicado for o ícone anterior, decrementar o mês atual em 1, caso contrário, incrementar em 1
-
-        if (icon.id === "prev" && (currMonth > currentMonth || currYear > currentYear)) {
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        if (icon.id === "prev" && currMonth > 0) {
             currMonth -= 1;
-        } else if (icon.id === "next" && (currMonth < 11 || currYear < currentYear + 1)) {
+        } else if (icon.id === "next" && currMonth < 11) {
             currMonth += 1;
         }
 
-        if (currMonth < 0 || currMonth > 11) { // se o mês atual for menor que 0 ou maior que 11
-            // criando uma nova data do ano e mês atuais e passando como valor de data
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // atualizando o ano atual com o ano da nova data
-            currMonth = date.getMonth(); // atualizando o mês atual com o mês da nova data
+        if (currMonth < 0 || currMonth > 11) {
+            date = new Date(currYear, currMonth, date.getDate());
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
         } else {
-            date = new Date(); // passando a data atual como valor de data
+            date = new Date();
         }
-        renderCalendar(); // chamando a função renderCalendar
-
-    })
+        calendarioInicio();
+    });
 });
 
-
-//Pegando o clique do usuário nas salas, adicionando o popup com a data selecionada e bloqueando o popup caso nenhum dia tenha sido selecionado
 const salas = document.getElementsByClassName("sala");
 const popup = document.getElementById("popup");
 const containerPopup = document.getElementById("container-popup");
-const diaSelecionado = document.getElementById("diaSelecionado");
+const diaSelecionado = document.getElementsByClassName("diaSelecionado");
 for (let i = 0; i < salas.length; i++) {
     salas[i].addEventListener('click', () => {
         if (dataSelecionada.trim() == "") {
@@ -132,16 +108,14 @@ for (let i = 0; i < salas.length; i++) {
         } else {
             popup.style.display = 'block'
             containerPopup.style.display = 'block'
-            diaSelecionado.textContent = dataSelecionada
+            for (let j = 0; j < diaSelecionado.length; j++) {
+                diaSelecionado[j].textContent = dataSelecionada
+            }
+            atualizarInputData();
         }
     })
-
 }
 
-// Fechando o popup quando:
-// X for clicado
-// clicar fora do popup
-// Cancelar
 const btnCancelar = document.getElementById('button-cancelar')
 const closePopup = document.getElementsByClassName('material-symbols-outlined')[0]
 closePopup.addEventListener('click', () => {
@@ -159,32 +133,74 @@ btnCancelar.addEventListener('click', () => {
     containerPopup.style.display = 'none'
 })
 
-
 document.getElementById('timeInicio').addEventListener('change', function () {
     checkTime(this);
+    validateTimeRange();
 });
 
 document.getElementById('timeFinal').addEventListener('change', function () {
     checkTime(this);
+    validateTimeRange();
 });
 
 function checkTime(input) {
     const time = input.value;
     const [hours, minutes] = time.split(':').map(Number);
-    if (hours > 18) {
-        alert('Horário inválido');
-        input.value = '';
-    }
-    else if (hours >= 18 && minutes >= 1) {
+
+    if ((hours >= 18 && minutes >= 0) || (hours < 7)) {
         alert('Horário inválido');
         input.value = '';
     }
 }
 
-//Definir recorrência
+function validateTimeRange() {
+    const timeInicio = document.getElementById('timeInicio').value;
+    const timeFinal = document.getElementById('timeFinal').value;
+
+    if (timeInicio && timeFinal) {
+        const [startHours, startMinutes] = timeInicio.split(':').map(Number);
+        const [endHours, endMinutes] = timeFinal.split(':').map(Number);
+
+        const startTime = startHours * 60 + startMinutes;
+        const endTime = endHours * 60 + endMinutes;
+
+        if (endTime <= startTime) {
+            alert('O horário de término deve ser maior que o horário de início');
+            document.getElementById('timeFinal').value = '';
+        }
+    }
+}
+
 const checkbox = document.getElementById('checkbox');
+const tabsRecorrencia = document.getElementById('tabs-recorrencia');
 checkbox.addEventListener('change', function (event) {
     if (event.target.checked) {
-        //
+        // Mudando display do Semanalmente e Diariamente
+        tabsRecorrencia.style.display = 'block';
+
+    } else {
+        tabsRecorrencia.style.display = 'none';
     }
+})
+
+// Quando o usuário clicar em reservar, fazemos a verificação e limpamos o popup
+const btnReservar = document.getElementById('button-reservar')
+btnReservar.addEventListener('click', () => {
+    const nomeReserva = document.getElementById('textInput').value
+    const timeInicio = document.getElementById('timeInicio').value
+    const timeTermino = document.getElementById('timeFinal').value
+    const checkbox = document.getElementById('checkbox');
+    const dataTermino = document.getElementById('dataTermino').value
+    if (nomeReserva.trim() == "" || timeInicio == "" || timeTermino == "") {
+        alert("Preencha todos os campos.")
+    }
+    else {
+        if (checkbox.checked) {
+            if (dataTermino == "") {
+                alert('Preencha a data final da reserva.')
+            }
+        }
+
+    }
+
 })
