e09b8cad4562e7081fe3c1571d1afc434e9d790a
beatriz.favero@germinare.org.br <beatriz.favero@germinare.org.br>
2024-06-27 07:01:38 -0300

    alterações filtros

---
+++ js/popups.js
@@ -0,0 +1,135 @@
+const checkbox = document.getElementById('checkbox');
+const tabsRecorrencia = document.getElementById('tabs-recorrencia');
+
+const resumoConfirmacao = document.getElementById('resumo-reserva')
+const popupConfirmacao = document.getElementById('popup-confirmacao')
+const salas = document.getElementsByClassName("sala");
+const popup = document.getElementById("popup");
+const containerPopup = document.getElementById("container-popup");
+const diaSelecionado = document.getElementsByClassName("diaSelecionado");
+for (let i = 0; i < salas.length; i++) {
+    salas[i].addEventListener('click', () => {
+        if (dataSelecionada.trim() == "") {
+            alert("Selecione uma data primeiro")
+        } else {
+            popup.style.display = 'block'
+            checkbox.checked = false
+            containerPopup.style.display = 'block'
+            tabsRecorrencia.style.display = 'none'
+            for (let j = 0; j < diaSelecionado.length; j++) {
+                diaSelecionado[j].textContent = dataSelecionada
+            }
+            atualizarInputData();
+        }
+    })
+}
+
+const btnCancelar = document.getElementById('button-cancelar')
+const closePopup = document.getElementsByClassName('material-symbols-outlined')[0]
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none'
+    containerPopup.style.display = 'none'
+})
+containerPopup.addEventListener('click', (event) => {
+    if (event.target == containerPopup) {
+        if (popup.style.display == 'block') {
+            popup.style.display = 'none'
+            containerPopup.style.display = 'none'
+        }
+        else {
+            alert('Confirme sua reserva antes de fechar.')
+        }
+
+    }
+})
+btnCancelar.addEventListener('click', () => {
+    popup.style.display = 'none'
+    containerPopup.style.display = 'none'
+})
+
+document.getElementById('timeInicio').addEventListener('change', function () {
+    checkTime(this);
+    validateTimeRange();
+});
+
+document.getElementById('timeFinal').addEventListener('change', function () {
+    checkTime(this);
+    validateTimeRange();
+});
+
+function checkTime(input) {
+    const time = input.value;
+    const [hours, minutes] = time.split(':').map(Number);
+
+    if ((hours >= 18 && minutes >= 0) || (hours < 7)) {
+        alert('Horário inválido');
+        input.value = '';
+    }
+}
+
+function validateTimeRange() {
+    const timeInicio = document.getElementById('timeInicio').value;
+    const timeFinal = document.getElementById('timeFinal').value;
+
+    if (timeInicio && timeFinal) {
+        const [startHours, startMinutes] = timeInicio.split(':').map(Number);
+        const [endHours, endMinutes] = timeFinal.split(':').map(Number);
+
+        const startTime = startHours * 60 + startMinutes;
+        const endTime = endHours * 60 + endMinutes;
+
+        if (endTime <= startTime) {
+            alert('O horário de término deve ser maior que o horário de início');
+            document.getElementById('timeFinal').value = '';
+        }
+    }
+}
+
+
+checkbox.addEventListener('change', function (event) {
+    if (event.target.checked) {
+        // Mudando display do Semanalmente e Diariamente
+        tabsRecorrencia.style.display = 'block';
+
+    } else {
+        tabsRecorrencia.style.display = 'none';
+    }
+})
+
+// Quando o usuário clicar em reservar, fazemos a verificação e limpamos o popup
+const nomeReservaEnviar = document.getElementById('nomeReserva')
+const horarioInicioEnviar = document.getElementById('horarioInicio')
+const horarioTerminoEnviar = document.getElementById('horarioTermino')
+const btnReservar = document.getElementById('button-reservar')
+btnReservar.addEventListener('click', () => {
+    const nomeReserva = document.getElementById('textInput').value
+    const timeInicio = document.getElementById('timeInicio').value
+    const timeTermino = document.getElementById('timeFinal').value
+    const checkbox = document.getElementById('checkbox');
+    const dataTermino = document.getElementById('dataTermino').value
+    if (nomeReserva.trim() == "" || timeInicio == "" || timeTermino == "") {
+        alert("Preencha todos os campos.")
+    }
+    else {
+        if (checkbox.checked) {
+            if (dataTermino == "") {
+                alert('Preencha a data final da reserva.')
+            }
+            else {
+                popup.style.display = 'none'
+                popupConfirmacao.style.display = 'block'
+                nomeReservaEnviar.innerText = nomeReserva
+                horarioInicioEnviar.innerText = timeInicio
+                horarioTerminoEnviar.innerText = timeTermino
+            }
+        }
+        else {
+            popup.style.display = 'none'
+            popupConfirmacao.style.display = 'block'
+            nomeReservaEnviar.innerText = nomeReserva
+            horarioInicioEnviar.innerText = timeInicio
+            horarioTerminoEnviar.innerText = timeTermino
+        }
+    }
+
+})
--- js/reservas.js
+++ js/reservas.js
@@ -2,9 +2,9 @@ const barra = document.getElementById('barra-lateral');
 const caminho = sessionStorage.getItem("img");
 barra.innerHTML = `<img src="${caminho}" alt="Imagem da barra lateral">`;
 
-// Nessa lista vai ficar o resumo do que o usuário selecionou.
 let reserva = []
 let dataSelecionada = ""
+let activeClass = ""
 
 // Funcionamento do calendário
 const daysTag = document.querySelector(".days"), // Seleciona o elemento HTML que contém os dias do calendário.
@@ -12,7 +12,6 @@ const daysTag = document.querySelector(".days"), // Seleciona o elemento HTML qu
     prevNextIcon = document.querySelectorAll(".icons span"), // Seleciona todos os elementos de ícone para navegação (anterior e próximo).
     prevIcon = document.getElementById("prev"), // Seleciona o ícone anterior especificamente
     nextIcon = document.getElementById("next"); // Seleciona o ícone próximo especificamente
-
 // Pegamos o ano e o mês atual (0-11)
 let date = new Date(),
     currYear = date.getFullYear(),
@@ -20,12 +19,11 @@ let date = new Date(),
     currentYear = currYear, // Armazenando o ano atual para restringir a navegação
     currentMonth = currMonth; // Armazenando o mês atual para restringir a navegação
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-
 const calendarioInicio = () => {
-    const firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    const firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+    const lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    const lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+
     let liTag = "";
 
     for (let i = firstDayofMonth; i > 0; i--) {
@@ -33,11 +31,11 @@ const calendarioInicio = () => {
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-        const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
         const dayOfWeek = new Date(currYear, currMonth, i).getDay();
-        const inactiveClass = (dayOfWeek === 0 || dayOfWeek === 6) ? "inactive" : "";
+        const inactiveClass = (dayOfWeek === 0 || dayOfWeek === 6) ? "inactive" : ""; //Definindo sábado e domingo como dias inativos e o resto com classe vazia
+        activeClass = (i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear()) ? "active" : "";
 
-        liTag += `<li class="${inactiveClass}">${i}</li>`;
+        liTag += `<li class="${inactiveClass} ${activeClass}">${i}</li>`;
     }
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
@@ -45,36 +43,24 @@ const calendarioInicio = () => {
     prevIcon.classList.toggle("hidden", currYear === currentYear && currMonth <= currentMonth);
     nextIcon.classList.toggle("hidden", currYear === currentYear && currMonth === 11);
 
-    document.querySelectorAll(".days li").forEach(day => {
-        day.addEventListener('click', () => {
-            if (!day.classList.contains("inactive")) {
-                document.querySelectorAll(".days li").forEach(d => d.classList.remove("active"));
-                day.classList.add("active");
-                reserva = [day.textContent];
-                dataSelecionada = `${day.textContent}/${currMonth + 1}/${currYear}`;
-                atualizarInputData();
-            }
-        });
-    });
 }
 
-const atualizarInputData = () => {
-    const dataSpan = document.querySelector(".diaSelecionado").textContent.trim();
-    if (dataSpan) {
-        const [dia, mes, ano] = dataSpan.split('/').map(Number);
-        const dataInicio = new Date(ano, mes - 1, dia);
-        const dataMin = new Date(dataInicio);
-        dataMin.setDate(dataMin.getDate() + 1);
-        const dataMax = new Date(dataInicio);
-        dataMax.setDate(dataMax.getDate() + 30);
-        const hoje = new Date();
+// const atualizarInputData = () => {
+//     const dataSpan = document.querySelector(".diaSelecionado").textContent.trim();
+//     if (dataSpan) {
+//         const [dia, mes, ano] = dataSpan.split('/').map(Number);
+//         const dataInicio = new Date(ano, mes - 1, dia);
+//         const dataMin = new Date(dataInicio);
+//         dataMin.setDate(dataMin.getDate() + 1);
+//         const dataMax = new Date(dataInicio);
+//         dataMax.setDate(dataMax.getDate() + 30);
+//         const hoje = new Date();
 
-        const inputDate = document.querySelector('input[type="date"]');
-        inputDate.min = dataMin.toISOString().split('T')[0];
-        inputDate.max = dataMax.toISOString().split('T')[0];
-
-    }
-}
+//         const inputDate = document.querySelector('input[type="date"]');
+//         inputDate.min = dataMin.toISOString().split('T')[0];
+//         inputDate.max = dataMax.toISOString().split('T')[0];
+//     }
+// }
 
 calendarioInicio();
 
@@ -97,110 +83,18 @@ prevNextIcon.forEach(icon => {
     });
 });
 
-const salas = document.getElementsByClassName("sala");
-const popup = document.getElementById("popup");
-const containerPopup = document.getElementById("container-popup");
-const diaSelecionado = document.getElementsByClassName("diaSelecionado");
-for (let i = 0; i < salas.length; i++) {
-    salas[i].addEventListener('click', () => {
-        if (dataSelecionada.trim() == "") {
-            alert("Selecione uma data primeiro")
-        } else {
-            popup.style.display = 'block'
-            containerPopup.style.display = 'block'
-            for (let j = 0; j < diaSelecionado.length; j++) {
-                diaSelecionado[j].textContent = dataSelecionada
+const diaSelecionado = document.getElementsByClassName("diaSelecionado"); 
+document.querySelectorAll(".days li").forEach(day => {
+    day.addEventListener('click', () => {
+        if (!day.classList.contains("inactive")) {
+            document.querySelectorAll(".days li").forEach(d => d.classList.remove("active"));
+            day.classList.add("active");
+            reserva = [day.textContent];
+            dataSelecionada = `${day.textContent}/${currMonth + 1}/${currYear}`;
+            for (let i = 0; i < diaSelecionado.length; i++) {
+                diaSelecionado[i].innerHTML = dataSelecionada;
             }
-            atualizarInputData();
+            // atualizarInputData();
         }
-    })
-}
-
-const btnCancelar = document.getElementById('button-cancelar')
-const closePopup = document.getElementsByClassName('material-symbols-outlined')[0]
-closePopup.addEventListener('click', () => {
-    popup.style.display = 'none'
-    containerPopup.style.display = 'none'
-})
-containerPopup.addEventListener('click', (event) => {
-    if (event.target == containerPopup) {
-        popup.style.display = 'none'
-        containerPopup.style.display = 'none'
-    }
-})
-btnCancelar.addEventListener('click', () => {
-    popup.style.display = 'none'
-    containerPopup.style.display = 'none'
-})
-
-document.getElementById('timeInicio').addEventListener('change', function () {
-    checkTime(this);
-    validateTimeRange();
+    });
 });
-
-document.getElementById('timeFinal').addEventListener('change', function () {
-    checkTime(this);
-    validateTimeRange();
-});
-
-function checkTime(input) {
-    const time = input.value;
-    const [hours, minutes] = time.split(':').map(Number);
-
-    if ((hours >= 18 && minutes >= 0) || (hours < 7)) {
-        alert('Horário inválido');
-        input.value = '';
-    }
-}
-
-function validateTimeRange() {
-    const timeInicio = document.getElementById('timeInicio').value;
-    const timeFinal = document.getElementById('timeFinal').value;
-
-    if (timeInicio && timeFinal) {
-        const [startHours, startMinutes] = timeInicio.split(':').map(Number);
-        const [endHours, endMinutes] = timeFinal.split(':').map(Number);
-
-        const startTime = startHours * 60 + startMinutes;
-        const endTime = endHours * 60 + endMinutes;
-
-        if (endTime <= startTime) {
-            alert('O horário de término deve ser maior que o horário de início');
-            document.getElementById('timeFinal').value = '';
-        }
-    }
-}
-
-const checkbox = document.getElementById('checkbox');
-const tabsRecorrencia = document.getElementById('tabs-recorrencia');
-checkbox.addEventListener('change', function (event) {
-    if (event.target.checked) {
-        // Mudando display do Semanalmente e Diariamente
-        tabsRecorrencia.style.display = 'block';
-
-    } else {
-        tabsRecorrencia.style.display = 'none';
-    }
-})
-
-// Quando o usuário clicar em reservar, fazemos a verificação e limpamos o popup
-const btnReservar = document.getElementById('button-reservar')
-btnReservar.addEventListener('click', () => {
-    const nomeReserva = document.getElementById('textInput').value
-    const timeInicio = document.getElementById('timeInicio').value
-    const timeTermino = document.getElementById('timeFinal').value
-    const checkbox = document.getElementById('checkbox');
-    const dataTermino = document.getElementById('dataTermino').value
-    if (nomeReserva.trim() == "" || timeInicio == "" || timeTermino == "") {
-        alert("Preencha todos os campos.")
-    }
-    else {
-        if (checkbox.checked) {
-            if (dataTermino == "") {
-                alert('Preencha a data final da reserva.')
-            }
-        }
-
-    }
-
-})
